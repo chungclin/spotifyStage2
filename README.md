e632440334f60dab1399981f190ce42df5f0ccdc
Chung Lin <chunglin87@gmail.com>
2018-06-25 21:50:29 -0400

    seeded events

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 
 The app has full functional Create, Read, Update and Delete.  However, I was not able to get the components to render upon making change to database.  Changes are reflected on the backend but may need to refresh the window for changes on the frontend to appear.
 
-For rendering the calendar, a seed script file was created to seed the database and render each month on the frontend.  
+For rendering the calendar, a seed script file was created to seed the database and render each month on the frontend. Please make sure to run the 'npm run seed' command to fully seed the database with the calendar so it can be rendered on the front end.
+
+
+****Please run 'npm run seed' to seed the database before running the application****
+
--- script/seed.js
+++ script/seed.js
@@ -20,6 +20,123 @@ const months = [
     { month: 'December' },
 ]
 
+const events = [
+    {
+        name: 'Job Interview', 
+        description: 'Spotify Job Interview',
+        startTime: '00:12',
+        endTime: '12:12',
+        dayId: 35
+    },
+    {
+        name: 'Cook Dinner', 
+        description: 'Chicken and Broccoli',
+        startTime: '12:43',
+        endTime: '12:59',
+        dayId: 45
+    },
+    {
+        name: 'Haircut', 
+        description: 'Cut the hair',
+        startTime: '13:12',
+        endTime: '14:45',
+        dayId: 35
+    },
+    {
+        name: 'Dinner', 
+        description: 'Eating with wife',
+        startTime: '17:12',
+        endTime: '18:12',
+        dayId: 34
+    },
+    {
+        name: 'Brunch with mom', 
+        description: 'Brunching with momma',
+        startTime: '10:12',
+        endTime: '12:12',
+        dayId: 33
+    },
+    {
+        name: 'Wedding', 
+        description: 'Eating prime rib',
+        startTime: '18:12',
+        endTime: '20:12',
+        dayId: 274
+    },
+    {
+        name: 'Guitar Tuning', 
+        description: 'Enter Address here',
+        startTime: '00:12',
+        endTime: '12:12',
+        dayId: 32
+    },
+    {
+        name: 'Buy school supples', 
+        description: 'pencils, pen, paper',
+        startTime: '00:12',
+        endTime: '12:12',
+        dayId: 91
+    },
+    {
+        name: 'Take medicine', 
+        description: 'Go to drug store',
+        startTime: '20:12',
+        endTime: '21:12',
+        dayId: 91
+    },
+    {
+        name: 'Play basketball', 
+        description: 'Go to ballcourt',
+        startTime: '12:12',
+        endTime: '16:12',
+        dayId: 91
+    },
+    {
+        name: 'Shopping with mom', 
+        description: 'Go shopping',
+        startTime: '12:12',
+        endTime: '12:42',
+        dayId: 32
+    },
+    {
+        name: 'New Years Day', 
+        description: 'New Years',
+        startTime: '16:12',
+        endTime: '22:12',
+        dayId: 1
+    },
+    {
+        name: 'Reading Party', 
+        description: 'Bookstore here',
+        startTime: '00:12',
+        endTime: '12:12',
+        dayId: 2
+    },
+    {
+        name: 'Buy cigarettes', 
+        description: 'Go to convenience',
+        startTime: '10:52',
+        endTime: '11:12',
+        dayId: 32
+    },
+    {
+        name: 'Final exam', 
+        description: 'Study!',
+        startTime: '10:12',
+        endTime: '12:12',
+        dayId: 36
+    },
+    {
+        name: 'Oil Change', 
+        description: 'Spotify Job Interview',
+        startTime: '12:32',
+        endTime: '12:52',
+        dayId: 40
+    }
+]
+
+
+
 const days = []
 
 function fillDays() {
@@ -86,6 +203,10 @@ const seed = () =>
     .then(() => 
     Promise.all(days.map(day => 
         Day.create(day))
+    ))
+    .then(() => 
+    Promise.all(events.map(event => 
+        Event.create(event))
     ))
 
     
