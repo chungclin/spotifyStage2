296f32d9cfc5d0482ee157dd31da0585b878efad
Chung Lin <chunglin87@gmail.com>
2018-07-17 22:26:42 -0400

    made backend tests

--- CalendarBackend/backendTests.js
+++ CalendarBackend/backendTests.js
@@ -36,96 +36,59 @@ describe('Order routes', () => {
       describe('POST', () => {
         beforeEach(async function() {
           await seed();
-          Trip.create({
-            planetName: 'Jupiter',
-            moonName: 'Io',
-            price: 1800,
-            startDate: '2017-08-09 04:05:02',
-            numberOfNights: 4,
-            description: 'One Way Trip',
-            imagePath:
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Io_highest_resolution_true_color.jpg/225px-Io_highest_resolution_true_color.jpg'
-          });
+          Event.create({
+            name: 'Buy Meat',
+            description: 'Go to the market',
+            startTime: '12:30',
+            endTime: '14:30',
+            dayId: 29
+          })
         });
   
-        // A user can add a new order
-        // Takes trip info, userId, Adds to order.
-        it('User can add an item to an order with POST api/users/{{usersId}}/orders,  getting a 201 response on success', () => {
+        it('User can add event with api/months/:monthid/day/:dayid/events,  getting a 201 response on success', () => {
           return request(app)
-            .post(`/api/users/1/orders`)
-            .send({ userId: 1, tripId: 2 })
+            .post(`/api/months/1/day/29/events`)
+            .send({ monthId: 1, dayId: 29 })
             .expect(201)
             .then(res => {
-              expect(res.body.moonName).to.equal('Io');
+              expect(res.body.name).to.equal('Buy Meat');
             });
         });
-  
-        // Can add a trip with more than the default number of guests
-        it('User can add an item to an order with POST api/users/{{usersId}}/orders,  getting a 201 response on success', () => {
-          return request(app)
-            .post(`/api/users/1/orders`)
-            .send({ tripId: 2, numberOfGuests: 15 })
-            .expect(201)
-            .then(() => Order.findById(1, { include: [Trip] }))
-            .then(order => {
-              expect(order.trips.length).to.equal(2);
-            });
-        });
-      }); // End POST Routes
-      describe('POST', () => {
-        beforeEach(async function() {
-          await seed();
-          await Trip.create({
-            planetName: 'Jupiter',
-            moonName: 'Io',
-            price: 1800,
-            startDate: '2017-08-09 04:05:02',
-            numberOfNights: 4,
-            description: 'One Way Trip',
-            imagePath:
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Io_highest_resolution_true_color.jpg/225px-Io_highest_resolution_true_color.jpg'
-          });
-          await request(app)
-            .post(`/api/users/1/orders`)
-            .send({ userId: 2, tripId: 2 });
-          console.log('Finished');
-        });
-  
-        it('Returns an error if posting a trip already on the order with POST api/users/{{usersId}}/orders', () => {
-          return request(app)
-            .post(`/api/users/1/orders`)
-            .send({ userId: 1, tripId: 2, numberOfGuests: 77 })
-            .expect(500);
-        });
-      }); // End POST Routes
-  
+        
+    // End Month Post Routes
+
       describe('order PUT and Delete routes', () => {
         beforeEach(async function() {
           await seed();
+          Event.create({
+            name: 'Buy Meat',
+            description: 'Go to the supermarket!!!',
+            startTime: '12:30',
+            endTime: '14:30',
+            dayId: 29,
+            id: 2
+          })
         });
   
-        it('User can update number of guests on a trip with PUT api/users/{{usersId}}/orders, getting a 200 response on success', () => {
+        it('User can update an event PUT api/months/:monthid/day/:dayid/events/:eventsid, getting a 200 response on success', () => {
           return request(app)
-            .put(`/api/users/1/orders`)
-            .send({ tripId: 1, numberOfGuests: 15 })
+            .put(`/api/months/1/days/29/events/1`)
+            .send({ monthId: 1, dayId: 29 })
             .expect(200)
-            .then(() => Order.findById(1, { include: [Trip] }))
-            .then(order => {
-              expect(order.trips[0].tripOrder.numberOfGuests).to.equal(15);
+            .then(event => {
+              expect(event.id).to.equal(2);
             });
         });
   
-        it('User can delete a trip with DELETE api/users/{{usersId}}/orders, getting a 204 response on success', () => {
+        it('User can delete a event with DELETE api/months/:monthid/day/:dayid/events/:eventid, getting a 204 response on success', () => {
           return request(app)
-            .delete(`/api/users/1/orders`)
-            .send({ tripId: 1 })
+            .delete(`/api/months/1/day/29/events/2`)
+            .send({ id: 2 })
             .expect(204)
-            .then(() => Order.findById(1, { include: [Trip] }))
-            .then(order => {
-              expect(order.trips.length).to.equal(0);
+            .then(deleted => {
+              expect(deleted.id).to.equal(null);
             });
         });
       });
-    }); // end describe('/api/orders')
-  }); // end describe('Order routes')
-  
+    }); 
+  }); 
